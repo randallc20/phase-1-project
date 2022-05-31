4ba5fb95a7b98017889cdc2b5a6bbf31b88ec59e
Teddy Milford <teddy.milford@gmail.com>
2022-05-31 13:19:16 -0600

    image generation html/js

--- src/homepage.js
+++ src/homepage.js
@@ -1,1 +1,33 @@
-"use strict"
+"use strict";
+
+let baseURL = "http://localhost:3000/artworks";
+
+const artContainerKey = document.getElementById("art-container");
+const evenColumnKey = document.getElementById("even");
+const oddColumnKey = document.getElementById("odd");
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadContent();
+});
+
+function loadContent() {
+  fetch(baseURL)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      data.forEach((artPiece, i) => {
+        imageGenerator(artPiece, i);
+      });
+    });
+}
+
+function imageGenerator(artPiece, i) {
+  let image = document.createElement("img");
+  image.src = artPiece.img_url;
+
+  if (i % 2 == 0) {
+    evenColumnKey.append(image);
+  } else {
+    oddColumnKey.append(image);
+  }
+}
